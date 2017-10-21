a350ccb85fb3489620d26afdc79ed22c7e559449
Hammad Awan <hammad_awan@hotmail.com>
2017-10-20 22:29:15 -0400

    Adding a "Create Meetup" Form | Vue.js + Vuetify + Firebase FULL PROJECT

--- src/components/MeetUp/CreateMeetUp.vue
+++ src/components/MeetUp/CreateMeetUp.vue
@@ -1,10 +1,95 @@
 <template>
-  <div>
-    <p>The Create Meetup Page</p>
-  </div>
+  <v-container>
+    <v-layout row wrap>
+      <v-flex xs12 sm6 offset-sm3>
+        <h4>Create a new Meetup</h4>
+      </v-flex>
+    </v-layout>
+    <v-layout row>
+      <v-flex xs12>
+        <form @submit.prevent="createMeetup">
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field id="title" name="title" label="Title" required v-model="title">
+              </v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field id="location" name="location" label="Location" required v-model="location">
+              </v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field id="imageUrl" name="imageUrl" label="Image URL" required v-model="imageUrl">
+              </v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <img :src="imageUrl" height="150">
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field id="description" name="description" label="Description" required multi-line v-model="description">
+              </v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-btn class="primary" :disabled="!formIsValid" type="submit">Create Meetup</v-btn>
+            </v-flex>
+          </v-layout>
+        </form>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
+export default {
+  data() {
+    return {
+      title: '',
+      location: '',
+      imageUrl: '',
+      description: ''
+    }
+  },
+  computed: {
+    formIsValid() {
+      return (
+        this.checkNotEmpty(this.title) &&
+        this.checkNotEmpty(this.location) &&
+        this.checkNotEmpty(this.imageUrl) &&
+        this.checkNotEmpty(this.description)
+      )
+    }
+  },
+  methods: {
+    checkNotEmpty(str) {
+      return str !== ''
+    },
+    createMeetup() {
+      if (!this.formIsValid) {
+        return
+      }
+
+      const meetup = {
+        id: this.$store.getters.loadedMeetups.length + 1,
+        title: this.title,
+        location: this.location,
+        imageUrl: this.imageUrl,
+        description: this.description,
+        date: new Date()
+      }
+      this.$store.dispatch('createMeetup', meetup)
+      this.$router.push({ name: 'Meetup', params: { id: meetup.id } })
+    }
+  }
+}
 </script>
 
 <style scoped>
--- src/store/index.js
+++ src/store/index.js
@@ -13,21 +13,36 @@ export default new Vuex.Store({
         date: new Date(2017, 7, 17)
       },
       {
-        imageUrl: 'http://www.telegraph.co.uk/content/dam/Travel/Destinations/Europe/France/Paris/paris-attractions-xlarge.jpg',
+        imageUrl:
+          'http://www.telegraph.co.uk/content/dam/Travel/Destinations/Europe/France/Paris/paris-attractions-xlarge.jpg',
         id: 2,
         title: 'Meetup in Paris',
         date: new Date(2017, 7, 19)
-      }],
+      }
+    ],
     user: {
       id: 'awanh',
       registeredMeetups: [1, 2]
     }
   },
   mutations: {
-
+    createMeetup(state, payload) {
+      state.loadedMeetups.push(payload)
+    }
   },
   actions: {
-
+    createMeetup({ commit }, payload) {
+      const meetup = {
+        id: payload.id,
+        title: payload.title,
+        location: payload.location,
+        imageUrl: payload.imageUrl,
+        description: payload.description,
+        date: payload.date
+      }
+      // Store to persistent source
+      commit('createMeetup', meetup)
+    }
   },
   getters: {
     loadedMeetups(state) {
@@ -39,8 +54,8 @@ export default new Vuex.Store({
       return getters.loadedMeetups.slice(0, 5)
     },
     loadedMeetup(state) {
-      return (meetupId) => {
-        return state.loadedMeetups.find((meetup) => {
+      return meetupId => {
+        return state.loadedMeetups.find(meetup => {
           return meetup.id === meetupId
         })
       }
